5c4afdbf659ce75d3785de44eaa22bc50ca3653a
Rostislav013 <46669977+Rostislav013@users.noreply.github.com>
2019-12-17 13:59:22 +0200

    upload file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FileUploader from  'react-firebase-file-uploader';
+import firebase from 'firebase';
+import firebaseConfig from './firebase.js';
+
+firebase.initializeApp(firebaseConfig);
+console.log(firebase)
+
+
+
+
+
+class App extends Component {
+
+  state = {
+    image: '',
+    imageURL: '',
+    progress: 0
+  }
+
+  handleUploadStart = () => {
+    this.setState({
+      progress: 0
+    })
+  }
+
+  handleUploadSuccess = filename => {
+    this.setState({
+      image: filename,
+      progress: 100
+    })
+
+    firebase.storage().ref('Images').child(filename).getDownloadURL()
+      .then(url => this.setState({
+        imageURL: url
+      }))
+      
+  }
+
+  render() {
+    console.log(this.state.progress)
+    return (
+      <div className="App">
+        <label>Progress</label>
+        <p>{this.state.progress}</p>
+        <FileUploader 
+          accept='image/*'
+          name='image'
+          storageRef={firebase.storage().ref('Images')}
+          onUploadStart={this.handleUploadStart}
+          onUploadSuccess={this.handleUploadSuccess}
+        />  
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCAm16zKKIjApYMU3pDskqYdH4-y_Gv3ZQ",
+    authDomain: "upload-files-firebase-7a8ba.firebaseapp.com",
+    databaseURL: "https://upload-files-firebase-7a8ba.firebaseio.com",
+    projectId: "upload-files-firebase-7a8ba",
+    storageBucket: "upload-files-firebase-7a8ba.appspot.com",
+    messagingSenderId: "258693073586",
+    appId: "1:258693073586:web:ae614c7972288846eb97ab",
+    measurementId: "G-4TN4JPHLS8"
+  };
+
+  export default firebaseConfig;
