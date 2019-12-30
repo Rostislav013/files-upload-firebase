617362383500e70760976e12026cfb9608fa8c0f
Rostislav013 <46669977+Rostislav013@users.noreply.github.com>
2019-12-30 22:24:00 +0200

    uploading

--- src/App.js
+++ src/App.js
@@ -7,19 +7,137 @@ import firebase from 'firebase';
 import firebaseConfig from './firebase.js';
 
 firebase.initializeApp(firebaseConfig);
-console.log(firebase)
+ 
 
 
-
+const INITIAL_STATE = []
 
 
 class App extends Component {
 
-  state = {
-    image: '',
-    imageURL: '',
-    progress: 0
+ 
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: '',
+      imageURL: '',
+      progress: 0,
+      listImg: []
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleGetImages = this.handleGetImages.bind(this);
+ 
+  }y
+
+  
+
+  componentDidMount() {
+    this.handleGetImages()
+    /*let ar = 
+    console.log(typeof(ar))
+    
+      
+       
+        this.setState({
+          listImg: this.handleGetImages()
+          });
+        console.log(this.state.listImg)*/
+      
   }
+
+
+
+
+   handleGetImages = () => {
+    let storageRef = firebase.storage().ref('Images');
+    let ar = []
+    
+     storageRef.listAll().then(  function(result) {
+      result.items.forEach( function(imageRef) {
+        // And finally display them
+        
+         displayImage(imageRef);
+       
+      });
+      }).catch(function(error) {
+      // Handle any errors
+    });
+
+    function displayImage(imageRef) {
+      imageRef.getDownloadURL().then(function(url) {
+           ar.push(url)
+       console.log(ar)
+
+
+      }   
+      )
+      
+     
+
+
+      }
+
+      this.setState({listImg: ar});
+     
+
+      setTimeout(function(){
+        this.setState({listImg: ar});
+   }.bind(this),3000);
+      
+   console.log(this.state.listImg)
+
+  /*  function displayImage(imageRef) {
+      imageRef.getDownloadURL().then(function(url) {
+        // TODO: Display the image on the UI
+        
+       let listImg = this.state.listImg.concat(url);
+       console.log(listImg)
+        return {
+          listImg 
+           
+        };
+        
+          
+         
+        
+
+           
+         
+
+  
+     
+             
+       
+
+      }).catch(function(error) {
+        // Handle any errors
+      });
+      console.log(this.state.listImg)
+     
+} */
+
+
+
+
+
+
+/*this.setState({
+  listImg: ar
+});*/
+
+
+
+
+
+   
+  }
+
+
+
+
+
 
   handleUploadStart = () => {
     this.setState({
@@ -32,27 +150,51 @@ class App extends Component {
       image: filename,
       progress: 100
     })
+    //this.handleGetImages()
+   
 
-    firebase.storage().ref('Images').child(filename).getDownloadURL()
-      .then(url => this.setState({
-        imageURL: url
-      }))
-      
+ /*    firebase.storage().ref('Images').child(filename).getDownloadURL()
+     .then(url => this.setState(state => {
+        let listImg = state.listImg.concat(url);
+        return {
+          listImg 
+           
+        };
+      })
+        
+        
+        
+       
+      )*/
+  
   }
+  
 
   render() {
-    console.log(this.state.progress)
+     
     return (
       <div className="App">
-        <label>Progress</label>
-        <p>{this.state.progress}</p>
-        <FileUploader 
+         
+        <label className="upload">
+        Choose file to upload
+        <FileUploader
+          hidden
           accept='image/*'
           name='image'
           storageRef={firebase.storage().ref('Images')}
           onUploadStart={this.handleUploadStart}
           onUploadSuccess={this.handleUploadSuccess}
-        />  
+        
+        /> 
+        </label>
+        <br/>
+         
+        <ul>
+          {this.state.listImg ? this.state.listImg.map(item => (
+            <li key={item}><a href={item}><img src={item}/></a></li>
+          )) : 'mdaaaa'}
+        </ul>
+        {/* this.state.image && <img src={this.state.imageURL} />*/ }
       </div>
     );
   }
